986a5acdba750352e7c15af0f39a71fbee88b3bb
Daisy T <daisyts@gmx.com>
2020-06-04 06:49:59 +0200

    automate it

--- main.go
+++ main.go
@@ -8,35 +8,16 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/jackc/pgx/v4/pgxpool"
 )
 
 var dbpool *pgxpool.Pool
 
-func createTable() error {
-	_, err := dbpool.Exec(context.Background(), "CREATE TABLE iss (timestamp TIMESTAMP GENERATED ALWAYS AS CURRENT_TIMESTAMP, position TEXT)")
-	return err
-}
-
 func insertData(position string) error {
 	_, err := dbpool.Exec(context.Background(), "INSERT INTO iss (position) VALUES ($1)", position)
 	return err
-}
-
-func listData() error {
-	rows, _ := dbpool.Query(context.Background(), "SELECT * FROM iss")
-
-	for rows.Next() {
-		var timestamp string
-		var position string
-		err := rows.Scan(&timestamp, &position)
-		if err != nil {
-			return err
-		}
-		fmt.Printf("%s, %s\n", timestamp, position)
-	}
-	return rows.Err()
 }
 
 type issInfo struct {
@@ -84,25 +65,17 @@ func main() {
 	}
 	defer dbpool.Close()
 
-	err = createTable()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to create table: %v\n", err)
-		os.Exit(1)
-	}
-
-	pos, err := getISSPosition()
-	if err != nil {
-		log.Fatal(err)
-	}
-	err = insertData(pos)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to insert data: %v\n", err)
-		os.Exit(1)
-	}
-
-	err = listData()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to list data: %v\n", err)
-		os.Exit(1)
+	for {
+		pos, err := getISSPosition()
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = insertData(pos)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Unable to insert data: %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Println("Sleeping for 10 seconds...")
+		time.Sleep(time.Second * 10)
 	}
 }
