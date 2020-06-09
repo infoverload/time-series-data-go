76654cab09e7588e0ea15919f92727606cbdf84d
Daisy T <daisyts@gmx.com>
2020-06-09 13:24:50 +0200

    change function to return string

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-	"strconv"
 )
 
 type issInfo struct {
@@ -16,38 +15,31 @@ type issInfo struct {
 	} `json:"iss_position"`
 }
 
-func getISSPosition() ([]float64, error) {
+func getISSPosition() (string, error) {
 	var i issInfo
-	a := make([]float64, 2)
 
 	response, err := http.Get("http://api.open-notify.org/iss-now.json")
 	if err != nil {
-		return nil, fmt.Errorf("unable to retrieve request: %v", err)
+		return "", fmt.Errorf("unable to retrieve request: %v", err)
 	}
 	defer response.Body.Close()
 
 	if response.StatusCode/100 != 2 {
-		return nil, fmt.Errorf("bad response status: %s", response.Status)
+		return "", fmt.Errorf("bad response status: %s", response.Status)
 	}
 
 	responseData, err := ioutil.ReadAll(response.Body)
 	if err != nil {
-		return nil, fmt.Errorf("unable to read response body: %v", err)
+		return "", fmt.Errorf("unable to read response body: %v", err)
 	}
 
 	err = json.Unmarshal(responseData, &i)
 	if err != nil {
-		return nil, fmt.Errorf("unable to unmarshal response body: %v", err)
+		return "", fmt.Errorf("unable to unmarshal response body: %v", err)
 	}
 
-	if long, err := strconv.ParseFloat(i.IssPosition.Longitude, 64); err == nil {
-		a[0] = long
-	}
-	if lat, err := strconv.ParseFloat(i.IssPosition.Latitude, 64); err == nil {
-		a[1] = lat
-	}
-
-	return a, nil
+	s := fmt.Sprintf("(%s, %s)", i.IssPosition.Longitude, i.IssPosition.Latitude)
+	return s, nil
 }
 
 func main() {
